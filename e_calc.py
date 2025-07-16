bfff929ce9da3f577d5ff9b8b535a47876baef65
dabaq01 <timothydabak@gmail.com>
2025-07-16 13:43:12 +0100

    .2f edit

--- e_calc.py
+++ e_calc.py
@@ -16,47 +16,47 @@ firstNumber = float (input ("first Number:"))
 #prompt the user for another number and store it
 secondNumber = float (input ("secondNumber:"))
 sum = float (firstNumber + secondNumber)
-print(f"{firstNumber} + {secondNumber}: {sum}")
+print(f"{firstNumber} + {secondNumber}: {sum:.2f}")
 
 print ("-----------")
 print ("Subtraction")
 print ("-----------")
 print("Please enter two numbers to subtract")
 #prompt the user for a number and store it
-firstNumber = float (input ("first Number:"))
+firstNumber = float (input ("First Number:"))
 #prompt the user for another number and store it
-secondNumber = float (input ("secondNumber:"))
+secondNumber = float (input ("Second Number:"))
 sub = float (firstNumber - secondNumber)
-print(f"{firstNumber} - {secondNumber}: {sub}")
+print(f"{firstNumber} - {secondNumber}: {sub:.2f}")
 
 print ("**************")
 print ("Multiplication")
 print ("**************")
 print("Please enter two numbers to multiply")
 #prompt the user for a number and store it
-firstNumber = float (input ("first Number:"))
+firstNumber = float (input ("First Number:"))
 #prompt the user for another number and store it
-secondNumber = float (input ("secondNumber:"))
+secondNumber = float (input ("Second Number:"))
 mul = float (firstNumber * secondNumber)
-print(f"{firstNumber} * {secondNumber}: {mul}")
+print(f"{firstNumber} * {secondNumber}: {mul:.2f}")
 
 print ("÷÷÷÷÷÷÷÷")
 print ("Division")
 print ("÷÷÷÷÷÷÷÷")
 print("Please enter two numbers to divide")
 #prompt the user for a number and store it
-firstNumber = float (input ("first Number:"))
+firstNumber = float (input ("First Number:"))
 #prompt the user for another number and store it
-secondNumber = float (input ("secondNumber:"))
+secondNumber = float (input ("Second Number:"))
 div = float (firstNumber / secondNumber)
-print(f"{firstNumber} / {secondNumber}: {div}")
+print(f"{firstNumber} / {secondNumber}: {div:.2f}")
 
 print ("^^^^^^^^^^^")
 print ("Exponential")
 print ("^^^^^^^^^^^")
 print("Please enter two numbers to run exponential")
 #prompt the user for a number and store it
-Number = float (input ("first Number:"))
-exp = float (firstNumber **2)
-print(f" Exponential of {Number} is {exp}")
-
+firstNumber = float (input ("First Number:"))
+SecondNumber = float (input ("Second Number:"))
+exp = float (firstNumber ** SecondNumber)
+print(f" Exponential of {firstNumber} ** {SecondNumber} is {exp:.2f}")
